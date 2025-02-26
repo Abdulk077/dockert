13e2c9ad2884634ed7835ec950d9742b592dfe9b
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-26 23:29:47 +0530

    Complete Second Step for using node in docker

---
+++ mynode/index.js
@@ -0,0 +1,18 @@
+// create templete for express
+const express = require('express'); 
+
+// create express app
+const app = express();
+
+// parse incoming JSON request bodies
+app.use(express.json());
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Express application."});
+});
+
+// listen server on port 3000
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
