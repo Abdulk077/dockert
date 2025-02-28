2161a97dd6bac397e72d9e2609b6233f73ee5467
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-28 23:38:24 +0530

    Using Docker Compose We Containerise our node app and serving using Dpcker

--- startapp/index.js
+++ startapp/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const DATABASE = "mongodb://mymongo:27017/testing";
-
+const express = require('express');
+const app = express();
 mongoose
         .connect(DATABASE)
         .then(() => {
@@ -10,3 +11,11 @@ mongoose
         .catch((err) => {
             console.log(err);
         });
+app.get('/', (req, res) => {
+    res.json({
+        message: 'You are now in root'
+    
+    })
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
